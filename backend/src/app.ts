67b4f3115b2a25d9ae3f651e0173d3a5775928e9
Herman100 <hermankwamebour30@gmail.com>
2025-05-13 15:13:03 +0000

    Replace JSON body parser with URL-encoded parser in app.ts for enhanced form data handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(morganMiddleware);
 
 export { app };
