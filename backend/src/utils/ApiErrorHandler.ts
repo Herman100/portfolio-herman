834bc863b1d4bd30d27c9f58055dfe0a6f899806
Herman100 <hermankwamebour30@gmail.com>
2025-05-14 11:13:56 +0000

    Refactor database connection module in db/index.ts to improve error handling and streamline connection logic

---
+++ backend/src/utils/ApiErrorHandler.ts
@@ -0,0 +1,29 @@
+class ApiErrorHandler extends Error {
+    stack?: any;
+    statusCode: any;
+    data: null;
+    errors: any;
+    success: boolean;
+
+    constructor(
+        statusCode: any,
+        stack: any = "",
+        message: string = "Unsuccessful api request",
+        errors: any = []
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export { ApiErrorHandler };
