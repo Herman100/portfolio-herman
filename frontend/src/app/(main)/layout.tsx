48288e08e59abc8f658e21af64b5d92a5933b672
Herman100 <hermankwamebour30@gmail.com>
2025-05-27 12:57:22 +0000

    feat: Implement admin login page and layout structure

---
+++ frontend/src/app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+// app/(main)/layout.tsx
+import NavHeader from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <NavHeader />
+      {children}
+      <Footer />
+    </>
+  );
+}
