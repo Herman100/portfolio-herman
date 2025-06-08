00e2888884af3f43dbe7fe926d2f28491ea7d979
Herman100 <hermankwamebour30@gmail.com>
2025-06-08 15:53:41 +0000

    feat: return request origin and cookies in response for enhanced debugging

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_BACKEND_URL ||
-  process.env.NEXT_PUBLIC_BACKEND_URL_LOCAL;
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+// process.env.NEXT_PUBLIC_BACKEND_URL_LOCAL;
 
 export const apiClient = axios.create({
   baseURL,
